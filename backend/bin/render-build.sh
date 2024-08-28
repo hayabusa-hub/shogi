a9183e4cc47098957f22d1de9e39d65650c24b78
hayabusa-hub <87015918+hayabusa-hub@users.noreply.github.com>
2024-08-28 19:29:05 +0900

    fix: フォルダ構成変更に伴うRenderへのデプロイ対応 && データベースに既にデータが存在する場合、デプロイに失敗する問題の修正 && deploymentをオフにする設定を削除

--- backend/bin/render-build.sh
+++ backend/bin/render-build.sh
@@ -2,8 +2,9 @@
 # exit on error
 set -o errexit
 
-bundle install --no-deployment
+cd backend
+bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-bundle exec rake db:migrate
+bundle exec rake db:migrate:reset
 bundle exec rails db:seed
